261da62449695206ce5d2240567a4deef97e33b0
tallapallysrujana <22311a05l6@cse.sreenidhi.edu.in>
2026-07-02 23:59:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (10.64%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,6 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-       Arrays.sort(nums);
+        int count0=0,count1=0,count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0) count0++;
+            if(nums[i]==1) count1++;
+            if(nums[i]==2) count2++;
+        }
+        //fill array accordingly
+        int index=0;
+        for(int i=0;i<count0;i++){
+            nums[index++]=0;
+        }
+        for(int i=0;i<count1;i++){
+            nums[index++]=1;
+        }
+        for(int i=0;i<count2;i++){
+            nums[index++]=2;
+        }
+
         }
     
 }
